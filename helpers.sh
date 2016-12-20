94d58f7ce9a5efa484f1fb0d256df7e3b25d21e0
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-12-20 17:59:33 +0800

    Add simple shell detection

--- helpers.sh
+++ helpers.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright (c) 2012 Adam Matan <adam@matan.name>
 # See the file license.txt for copying permission.
 
 # verified on RHEL 6.6
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ `which $SHELL` = "/bin/ksh" ]; then
+    DIR="$( cd "$( dirname "_[2]" )" && pwd )"
+elif [ `which $SHELL` = "/bin/bash" ]; then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
 
 source $DIR/aliases/aliases.sh
 source $DIR/prompt/prompt_two_lines_blue.sh
