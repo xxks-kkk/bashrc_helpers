f9f8ac96660e9f6d694743bcc9aece0115f99ae6
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-12-20 12:46:47 +0800

    Update alias to mine

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -1,14 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 # Copyright (c) 2012 Adam Matan <adam@matan.name>
+# Copyright (c) 2016 Zeyuan Hu <ferrishu3886@gmail.com>
 # See the file license.txt for copying permission.
 
-alias gerp='grep'  		     # Helps with Dislexya
-alias rsyn='rsync -avzr --progress'  # I want this for every remote sync
-alias wgeto='wget -qO- '	     # wget to standard output
+# Command overrides
 alias tailf='tail -f '		     
+alias pbcopy='xsel --clipboard --input'                          ## copy input file to clipboard
+alias pbpaste='xsel --clipboard --input'                         ## paste clipboard to input file
+alias em='emacs -nw'                                             ## quickly fire up emacs
+alias sshconfig='vi $HOME/.ssh/config'                           ## see current ssh config
+alias home="cd $HOME"                                            ## cd into home
+alias bashrc="vi /home/zeyuan/.bashrc"                           ## quickly edit .bashrc
 
-# Ubuntu
-alias srch='apt-cache search'
-alias sair='sudo apt-get install'
+
+
+# git related
+alias quickdel="git ls-files --deleted -z | xargs -0 git rm"     ## rm the deleted files from git control
+
+# LaTex related
+alias latexmgrgui='sudo env PATH="$PATH" tlmgr --gui'            ## bring up latex manager gui
+alias lsudo='sudo env PATH="$PATH"'                              ## use speciall sudo to use tlmgr
+
+
+# Misc tweaks
+/usr/bin/setxkbmap -option "ctrl:swapcaps"                       ## switch tabs with caps
+
 
