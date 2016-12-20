b7f70d5eeeda1fb3a3c1b0faf1787b5604101fc6
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-12-20 14:17:10 +0800

    customize to mine

--- history/history_settings.sh
+++ history/history_settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Copyright (c) 2016 Zeyuan Hu <ferrishu3886@gmail.com>
 # Copyright (c) 2012 Adam Matan <adam@matan.name>
 # See the file license.txt for copying permission.
 
@@ -9,7 +10,7 @@ export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
 export HISTSIZE=1000000
 export HISTFILESIZE=1000000
 export HISTTIMEFORMAT='%F %T '
-shopt -s histappend                      # append to history, don't overwrite it
+shopt -s histappend                      # when the shell exists, append to history file instead of overwriting it
 
 # Save and reload the history after each command finishes
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
