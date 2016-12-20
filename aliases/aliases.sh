ac6b4c9730d78e26010f05c24ba80fb604856351
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-12-20 15:01:50 +0800

    Finish port the alias

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -6,24 +6,40 @@
 
 # Command overrides
 alias tailf='tail -f '		     
-alias pbcopy='xsel --clipboard --input'                          ## copy input file to clipboard
-alias pbpaste='xsel --clipboard --input'                         ## paste clipboard to input file
-alias em='emacs -nw'                                             ## quickly fire up emacs
-alias sshconfig='vi $HOME/.ssh/config'                           ## see current ssh config
-alias home="cd $HOME"                                            ## cd into home
-alias bashrc="vi /home/zeyuan/.bashrc"                           ## quickly edit .bashrc
-
-
+alias pbcopy='xsel --clipboard --input'                                                  ## copy input file to clipboard
+alias pbpaste='xsel --clipboard --input'                                                 ## paste clipboard to input file
+alias sshconfig='vi $HOME/.ssh/config'                                                   ## see current ssh config
+alias home="cd $HOME"                                                                    ## cd into home
+alias bashrc="vi /home/zeyuan/.bashrc"                                                   ## quickly edit .bashrc
+alias cleanup="find . -type f -name '#*#' -delete; find . -type f -name '*~' -delete"    ## recursively cleanup for a repo
 
 # git related
-alias quickdel="git ls-files --deleted -z | xargs -0 git rm"     ## rm the deleted files from git control
+alias quickdel="git ls-files --deleted -z | xargs -0 git rm"                             ## rm the deleted files from git control
 
 # LaTex related
-alias latexmgrgui='sudo env PATH="$PATH" tlmgr --gui'            ## bring up latex manager gui
-alias lsudo='sudo env PATH="$PATH"'                              ## use speciall sudo to use tlmgr
+alias latexmgrgui='sudo env PATH="$PATH" tlmgr --gui'                                    ## bring up latex manager gui
+alias lsudo='sudo env PATH="$PATH"'                                                      ## use speciall sudo to use tlmgr
 
+# emacs related
+export ALTERNATE_EDITOR=""
+export EDITOR=emacsclient
+alias em='emacs -nw'                                                                     ## quickly fire up emacs
 
 # Misc tweaks
-/usr/bin/setxkbmap -option "ctrl:swapcaps"                       ## switch tabs with caps
-export LSCOLORS=gxfxcxdxbxegedabagacad
+/usr/bin/setxkbmap -option "ctrl:swapcaps"                                               ## switch tabs with caps
+alias left='xrandr --output VGA-0 --rotate left'                                         ## rotate screen left
+alias normal='xrandr --output VGA-0 --rotate normal'                                     ## rotate screen normal
 
+# Enable color support of ls and also add handy aliases
+export LSCOLORS=gxfxcxdxbxegedabagacad
+if [ -x /usr/bin/dircolors ]; then
+   test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+   export LS_COLORS=$LS_COLORS:"*.sh=01;37;41"
+   alias ls='ls --color=auto'
+   #alias dir='dir --color=auto'
+   #alias vdir='vdir --color=auto'
+
+   alias grep='grep --color=auto'
+   alias fgrep='fgrep --color=auto'
+   alias egrep='egrep --color=auto'
+fi
--- helpers.sh
+++ helpers.sh
@@ -3,9 +3,12 @@
 # Copyright (c) 2012 Adam Matan <adam@matan.name>
 # See the file license.txt for copying permission.
 
+# verified on RHEL 6.6
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/aliases/aliases.sh
 source $DIR/prompt/prompt_two_lines_blue.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/history/history_settings.sh
 
