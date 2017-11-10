4954f9372df71e15d3e02c98f537c836d794a2f3
Zeyuan Hu <ferrishu3886@gmail.com>
2017-11-10 15:35:57 -0600

    add ~/bin to PATH

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -64,4 +64,5 @@ elif [[ $platform == 'mac' ]]; then
     #unset PYTHONPATH;                                                                       ## This is for python3
     export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages                     ## Do this "export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages" before using python2. Now the system by default will use python2
     alias tensorboard2="python2 /usr/local/lib/python2.7/site-packages/tensorboard/main.py"  ## tensorboard is linked with my Python3. This alias is a shorthand to run python2 tensorboard on my system
+    export PATH=$PATH:$HOME/bin
 fi
