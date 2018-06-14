d36fe88408b3a4ff0344ad14eb695e6d530f3843
Zeyuan Hu <ferrishu3886@gmail.com>
2018-06-14 10:16:52 -0500

    Update backup_history.sh

--- backup_history.sh
+++ backup_history.sh
@@ -2,6 +2,7 @@
 
 # Start a cron job to back up the .persistent_history file
 # to a safe place
+# Ref: https://code.tutsplus.com/tutorials/scheduling-tasks-with-cron-jobs--net-8800
 
 SAFE_PLACE=/work/
 CRONJOB="15 * * * * cp ~/.persistent_history /work > /dev/null 2>&1"
