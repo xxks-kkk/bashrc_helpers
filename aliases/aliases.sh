1629e9c4c9aca237b1ef9dce1059b8adfa4b0074
Zeyuan Hu <ferrishu3886@gmail.com>
2018-10-08 23:20:52 -0500

    Updates

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -27,6 +27,38 @@ alias cleanup="find . -type f -name '#*#' -delete; find . -type f -name '*~' -de
 alias phgrep='cat ~/.persistent_history|grep --color'                                    ## query .persistent_history file for a command
 alias hgrep='history|grep --color'                                                       ## query history file for a command
 alias ls='ls -G'                                                                         ## colorize ls output
+alias ..="cd .."                                                                         ## same as `up 2` below
+alias ...="cd ../.."                              
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias treed="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'" ## Expand current directory structure in tree form
+alias logs="find /var/log -type f -exec file {} \; | grep 'text' | cut -d' ' -f1 | sed -e's/:$//g' | grep -v '[0-9]$' | xargs tail -f" ## Tail all logs in /var/log
+
+# Utility functions
+up() { cd $(eval printf '../'%.0s {1..$1}) && pwd; }                                     ## Move 'up' so many directories instead of using several cd ../../, etc.
+extract () {                                                                             ## Easy way to extract archives
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xvjf $1    ;;
+            *.tar.gz)    tar xvzf $1    ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar x $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xvf $1     ;;
+            *.tbz2)      tar xvjf $1    ;;
+            *.tgz)       tar xvzf $1    ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "don't know how to extract '$1'..." ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
+function weather() {                                                                     ## Get weather
+    echo ""; w3m http://www.usairnet.com/weather/forecast/local/?pands=$1 | grep -A 10 "${2^^}"; echo ""; curl -s http://wttr.in/$2;
+}
 
 # git related
 alias quickdel="git ls-files --deleted -z | xargs -0 git rm"                             ## rm the deleted files from git control
@@ -75,11 +107,16 @@ elif [[ $platform == 'mac' ]]; then
     alias tensorboard2="python2 /usr/local/lib/python2.7/site-packages/tensorboard/main.py"  ## tensorboard is linked with my Python3. This alias is a shorthand to run python2 tensorboard on my system
     export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home
     alias conda="/Users/zeyuan/anaconda2/bin/conda"
-    alias zyh="cd /Volumes/MacSSD/Users/zyh"                                                ## The home directory for my mac ssd disk
+    alias zyh="cd /Volumes/MacSSD/Users/zyh"                                                 ## The home directory for my mac ssd disk
+    alias wine="/Applications/Wine Stable.app/Contents/Resources/wine/bin/wine"            
 elif [[ $platform == 'utcs' ]]; then
     alias bld="cd /u/zeyuanhu/Documents/norman-439/src/threads/build"
     export GOPATH=$HOME/go
     alias go2="/lusr/opt/go-go1.10.1/bin/go"
+    sbs() {                                                                                  ## List by file size in current directory
+        du -b --max-depth 1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", $1>=2**30? ($1/2**30, "G"): $1>=2**20? ($1/2**20, "M"): $1>=2**10? ($1/2**10, "K"): ($1, "")}e';
+    }
+    alias port='netstat -tulanp'                                                             ## Show active ports
 elif [[ $platform == 'windows' ]]; then
     # This part assumes to work with git bash
     alias python='winpty python.exe'
