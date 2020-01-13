356a1d4dc289dcf00c28079e39399f9191715d17
zeyuanhu <zeyuanhu@amazon.com>
2020-01-13 21:50:20 +0000

    Add amazon specific tweak

---
+++ aliases/amazon.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Amazon Daily Setup Process (leave here as reminder)
+#if [ -e $HOM/bin/mwdaily.py ];then
+#    python3 $HOME/bin/mwdaily.py
+#else
+#    mwinit -o
+#fi
+
+alias bb="brazil-build"
+alias gc="git config --global user.name \"zeyuanhu\"; git config --global user.email \"zeyuanhu@amazon.com\"; git commit"
