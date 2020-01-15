8037bab6d939429767bfff216a8f850b5413803b
zeyuanhu <zeyuanhu@amazon.com>
2020-01-15 23:06:38 +0000

    Update AFPCLI setting

--- aliases/amazon.sh
+++ aliases/amazon.sh
@@ -9,3 +9,8 @@
 
 alias bb="brazil-build"
 alias gc="git config --global user.name \"zeyuanhu\"; git config --global user.email \"zeyuanhu@amazon.com\"; git commit"
+
+# AFPCLI related variables
+export PATH="$PATH:/apollo/env/AWSForecastingPlatformServiceCLI/bin"
+export AFP_ODIN_CREDENTIALS=com.amazon.credentials.isengard.404791104858.user/AFP-User
+export AFP_REGION=us-east-1
