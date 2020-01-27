6f042c24d684184d97084f3993cbd761d7e01168
xxks-kkk <ferrishu3886@gmail.com>
2020-01-27 15:00:14 -0800

    Update

--- aliases/amazon.sh
+++ aliases/amazon.sh
@@ -7,12 +7,17 @@
 #    mwinit -o
 #fi
 
+alias bz="brazil"
 alias bb="brazil-build"
 alias gca="git config --global user.name \"zeyuanhu\"; git config --global user.email \"zeyuanhu@amazon.com\"; git commit"
 alias lt="cd /home/zeyuanhu/workplace/local_test/src/Local_tests"
-alias bbr=" brazil-build clean && brazil-build release"
+alias bbr="brazil-build clean && brazil-build release"
+alias int="cd ~/workplace/afp/src/AWSForecastingPlatformServiceModel/ && brazil-build clean && brazil-build release \
+&& cd ~/workplace/afp/src/AWSForecastingPlatformCommonJavaModel && brazil-build clean && brazil-build release \
+&& cd ~/workplace/afp/src/AWSForecastingPlatformCommon/ && brazil-build clean && brazil-build release" # this command builds necessary packages to make intellij work properly
 
 # AFPCLI related variables
 export PATH="$PATH:/apollo/env/AWSForecastingPlatformServiceCLI/bin"
 export AFP_ODIN_CREDENTIALS=com.amazon.credentials.isengard.404791104858.user/AFP-User
 export AFP_REGION=us-east-1
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_231.jdk/Contents/Home
