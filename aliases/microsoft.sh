06f436629e020ba831ad604b3ab2df495243a9cb
xxks-kkk <ferrishu3886@gmail.com>
2022-06-03 14:58:34 -0700

    tweak author name for microsoft

--- aliases/microsoft.sh
+++ aliases/microsoft.sh
@@ -1,1 +1,1 @@
-alias gcm="git config --global user.name \"zeyuanhu\"; git config --global user.email \"T-zeyuanhu@microsoft.com\"; git commit"
+alias gcm="git config --global user.name \"Zeyuan Hu\"; git config --global user.email \"T-zeyuanhu@microsoft.com\"; git commit"
