f7e8385996c3ea60a6ab8806b25731e6d5044c3f
xxks-kkk <ferrishu3886@gmail.com>
2025-05-27 14:57:56 -0700

    Add bytedance profile

---
+++ aliases/bytedance.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Bytedance Daily Setup Process (leave here as reminder)
+# kinit zeyuan.hu
+
+alias gcb="git config --global user.name \"zeyuan.hu\"; git config --global user.email \"zeyuan.hu@bytedance.com\"; git commit"
